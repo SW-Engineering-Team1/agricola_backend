bcac32032ed5b9f7f3e7cd5350d8646d1df9a80a
mj.kim <alswo9853@gmail.com>
2023-04-11 00:05:23 +0900

    commit node express test activity with REST API & db connection

---
+++ minjae/index.js
@@ -0,0 +1,42 @@
+// index.js
+const express = require('express');
+const app = express();
+
+const { Client } = require("pg");
+const client = new Client({
+  user: "vraptor",
+  host: "192.168.0.5",
+  database: "vraptor",
+  password: "vraptor123!",
+  port: 5432,
+});
+
+// for parsing application/json
+app.use(express.json());
+
+const user = [];
+
+app.get('/', function (req, res) {
+    client.connect();
+    client.query("SELECT * FROM test").then(res => {
+        console.log(res.rows);
+        client.end();
+    }).catch(e => 
+      console.error(e.stack)
+    );
+    return res.send('hello world');
+})
+
+app.get('/user', function (req, res) {
+  return res.send(user);
+})
+
+app.post('/user', function (req, res) {
+  console.log(req.body);
+  user.push(req.body);
+  return res.send(user);
+})
+
+app.listen(3000, function () {
+    console.log('server listening on port 3000');
+})
