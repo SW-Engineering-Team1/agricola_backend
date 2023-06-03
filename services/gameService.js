4becc02f5d97c08baff70b5f614860f5e86314af
BaeGayeon <ebgy918@gmail.com>
2023-06-03 20:40:29 +0900

    feat: 빵 굽기 시 주요 설비 확인하도록 구현

--- services/gameService.js
+++ services/gameService.js
@@ -792,4 +792,17 @@ module.exports = {
       return baseResponse.DB_ERROR;
     }
   },
+  findUsedMainFacility: async function (userId) {
+    try {
+      let findResult = await GameStatus.findOne({
+        where: {
+          userId,
+        },
+      });
+      return findResult.dataValues.usedMainFacilityCard;
+    } catch (err) {
+      console.log(err);
+      return baseResponse.DB_ERROR;
+    }
+  },
 };
